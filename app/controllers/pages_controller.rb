5c8c9be3bc942d8d6d3247313cfb7850d49ca3c3
ljsanvitale <lsanvitale+github@gmail.com>
2018-04-20 17:55:17 -0300

    2nd commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
 
   def index
+    @main_articles=Article.take(7)
+    #@articles_list=Article.all(:limit => 5, :offset => 5)
   end
 
 end
---
+++ config/initializers/constants.rb
@@ -0,0 +1,1 @@
+ARTICLE_POSITIONS= ['col-xs-8', 'col-xs-4','col-xs-6','col-xs-6','col-xs-4','col-xs-4','col-xs-4']
