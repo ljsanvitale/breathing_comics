b43e4ee17acaba44ae5e49a8e476c698636b00aa
ljsanvitale <lsanvitale+github@gmail.com>
2018-05-10 14:07:23 -0300

    added buy url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180503344356) do
+ActiveRecord::Schema.define(version: 20181003344356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20180503344356) do
     t.string "publisher"
     t.datetime "published_at"
     t.string "cover_price"
+    t.string "url"
     t.index ["author_id"], name: "index_articles_on_author_id"
   end
 
