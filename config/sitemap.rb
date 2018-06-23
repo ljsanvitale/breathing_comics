46e241d4792b203bb8e8a6a9b0d99e277e665280
ljsanvitale <lsanvitale+github@gmail.com>
2018-06-23 12:28:10 -0300

    sitemap

---
+++ config/sitemap.rb
@@ -0,0 +1,38 @@
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "https://www.breathingcomics.com"
+
+SitemapGenerator::Sitemap.create do
+  add root_path
+  add news_path
+  add reviews_path
+  add articles_path
+  Article.find_each do |art|
+    add page_path(art), :changefreq => 'weekly', :lastmod => art.updated_at
+  end
+  Tag.find_each do |tag|
+    add tag_page_path(tag), :changefreq => 'weekly', :lastmod => tag.updated_at
+  end
+
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically for you.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+  #
+  # Examples:
+  #
+  # Add '/articles'
+  #
+  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
+  #
+  # Add all articles:
+  #
+  #   Article.find_each do |article|
+  #     add article_path(article), :lastmod => article.updated_at
+  #   end
+end
